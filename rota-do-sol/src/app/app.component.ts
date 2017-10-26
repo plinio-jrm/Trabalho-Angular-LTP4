44b3ce609b5e7776f526ede2d67ed52630f6ddfc
Plinio.jrm <plinio.jrm@gmail.com>
2017-10-26 13:33:03 -0200

    Pequena mudança no prefixo para "sol" e da tag principal pra "sol-root".

--- rota-do-sol/src/app/app.component.ts
+++ rota-do-sol/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sol-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
